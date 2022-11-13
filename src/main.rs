c5772dcfc71bb1c1be4985cdab77fe06645f44c1
Pravinkumar Singh <pravinkumarosingh@gmail.com>
2022-11-13 10:19:26 +0530

    data types and variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+fn add(num_one:i8, num_two:i8) -> i8{
+    return num_one + num_two;
+}
+
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut my_name = "Pravin";
+    println!("Hello {my_name}");
+    my_name = "ranga";
+    println!("Hello, world! {my_name}");
+
+    let num_one:i8= 1;
+    let num_two:i8 = 2;
+
+    let total:i8 = add(num_one, num_two);
+    println!("Addition is {total}")
+
 }
