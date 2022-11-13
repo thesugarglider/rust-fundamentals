1ffffda3b8f4a4e82ccdd5391089f21fc81ee831
Pravinkumar Singh <pravinkumarosingh@gmail.com>
2022-11-13 10:23:27 +0530

    println modified

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,12 @@ fn main() {
     let mut my_name = "Pravin";
     println!("Hello {my_name}");
     my_name = "ranga";
-    println!("Hello, world! {my_name}");
+    println!("Hello, world! {}",my_name);
 
     let num_one:i8= 1;
     let num_two:i8 = 2;
 
     let total:i8 = add(num_one, num_two);
-    println!("Addition is {total}")
+    println!("Addition is {}",total);
 
 }
