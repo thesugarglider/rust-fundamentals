96f9f1cd8326dd633b6376df0364e3e66cedfbfb
Pravinkumar Singh <pravinkumarosingh@gmail.com>
2022-11-13 16:58:31 +0530

    conditional statements

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+
+//functions in rust
 fn add(num_one:i8, num_two:i8) -> i8{
     return num_one + num_two;
 }
@@ -5,15 +7,49 @@ fn add(num_one:i8, num_two:i8) -> i8{
 
 
 fn main() {
-    let mut my_name = "Pravin";
+
+    //variables
+    let name = "ranga";             //immutable variable
+    println!("{}",name);
+
+    let mut my_name = "Pravin";     //mutable variable
+
     println!("Hello {my_name}");
     my_name = "ranga";
     println!("Hello, world! {}",my_name);
 
-    let num_one:i8= 1;
-    let num_two:i8 = 2;
+    let num_one:i8= 10;
+    let num_two:i8 = 10;
 
-    let total:i8 = add(num_one, num_two);
+    let mut total:i8 = add(num_one, num_two);
     println!("Addition is {}",total);
+
+
+
+    //conditional statements
+
+    let mut free_shipping = false;
+
+    //if..else
+    if total > 50 {
+        println!("You are qualified");
+        free_shipping = true;
+    }
+    else if total > 30 {
+        println!("you are going good");
+    }
+    else {
+        println!("need to work hard");
+    }
+
+    //match expressions
+    
+
+    match free_shipping {
+        true => total += 0,
+        false => total += 5
+
+    }
+    println!("{}",total);
 
 }
